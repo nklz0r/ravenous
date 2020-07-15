954303b1a15787369c2a85f1387c3c5819ebdede
nklz0r <68295861+nklz0r@users.noreply.github.com>
2020-07-16 01:46:46 +0300

    Update App.js

--- components/App/App.js
+++ components/App/App.js
@@ -5,19 +5,6 @@ import SearchBar from '../SearchBar/SearchBar';
 import { Yelp } from '../../Util/Yelp';
 
 
-// const business = {
-//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-// name: 'MarginOtto Pizzeria',
-// address: '1010 Paddington Way',
-// city: 'Flavortown',
-// state: 'NY',
-// zipCode: '10101',  
-// category: 'Italian',
-// rating: 4.5,
-// reviewCount: 90
-// }//* hard coded business obj until Yelp was imported
-
-// const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
   constructor(props) {
@@ -51,12 +38,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// Below is a list of some potential features to add to Ravenous:
-
-// Make addresses clickable and have them open the address in Google Maps in a new tab
-// Make images clickable and have them open the business’ website in a new tab
-// Clicking on a different sorting option automatically requeries the Yelp API, rather than having to manually click “Let’s Go” again - DONE
-// Implement your own type of sort (for example, by entering a distance or radius from a central location)
-// Allow you to search by pressing “Enter” (or “Return”) on your keyboard, as opposed to manually clicking - DONE
-// Add autocompletion of addresses to the “Location” input  
